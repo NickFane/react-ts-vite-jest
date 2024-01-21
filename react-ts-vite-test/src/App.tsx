d9d63663034bf32e1943ddafc533f998d9c00f01
Nick Fane <nickgfane@gmail.com>
2024-01-21 15:31:13 +1000

    working template

---
+++ react-ts-vite-test/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css' // I recommend importing CSS, to make sure your moduleNameMapper is working
+
+function App() {
+
+  return (
+    <>
+      <h1>React Vite Jest</h1>
+    </>
+  )
+}
+
+export default App
